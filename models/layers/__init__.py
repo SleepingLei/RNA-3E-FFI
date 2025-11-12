6400d200d82587fd6f90e8e86188c895f4ae6dd9
“SleepingLei” <dwlei@stu.pku.edu.cn>
2025-11-12 14:06:34 +0800

    fix

--- models/layers/__init__.py
+++ models/layers/__init__.py
@@ -3,11 +3,12 @@ E(3) Equivariant Layers for RNA-3E-FFI
 """
 from .radial_basis import BesselBasis, PolynomialCutoff
 from .message_passing import ImprovedE3MessagePassingLayer
-from .normalization import EquivariantLayerNorm
+from .normalization import EquivariantLayerNorm, EquivariantRMSNorm
 
 __all__ = [
     'BesselBasis',
     'PolynomialCutoff',
     'ImprovedE3MessagePassingLayer',
-    'EquivariantLayerNorm'
+    'EquivariantLayerNorm',
+    'EquivariantRMSNorm'
 ]
