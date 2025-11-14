25ea61372d06946c2923633ce32280217a683b6e
SleepingLei <dwlei@stu.pku.edu.cn>
2025-11-14 18:51:15 +0800

    remote

--- scripts/train_physics_v3.sh
+++ scripts/train_physics_v3.sh
@@ -19,11 +19,11 @@ python scripts/04_train_model.py \
     --save_every 5 \
     --loss_fn mse --monitor_gradients \
     --num_layers 6 --use_layer_norm --dropout 0.1 \
-    --output_dir models/physics_v3_6_0.1_new_improve_multihead_retry_no_schedular \
+    --output_dir models/physics_v3_6_0.1_new_improve_multihead_retry_plateau_scheduler \
     --use_ddp --world_size 4 --splits_file data/splits/filtered_splits.json \
     --use_v3_model --num_attention_heads 8 --grad_clip 1.0 --initial_angle_weight 0.33 --initial_dihedral_weight 0.33 --initial_nonbonded_weight 0.33 \
     --use_enhanced_invariants \
-    --scheduler none \
+    --scheduler plateau \
     #--use_improved_layers \
     #--use_physics_loss \
     #--resume --checkpoint models/physics_v3_6_0.1/checkpoint_epoch_120.pt
